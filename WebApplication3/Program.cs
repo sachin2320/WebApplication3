b97941cb27dcb11d746d95bd136deed1b2b5bd97
sachin gupta <sachin2320@gmail.com>
2020-10-06 20:05:46 +0530

    ss

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -14,7 +14,6 @@ namespace WebApplication3
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
