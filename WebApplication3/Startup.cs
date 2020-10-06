184baba47cb8e87cdf12922807e4d224924a33b0
sachin gupta <sachin2320@gmail.com>
2020-10-06 23:25:06 +0530

    master1

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -16,6 +16,7 @@ namespace WebApplication3
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
